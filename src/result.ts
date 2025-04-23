f18d41c27a6bdbbf28dc412a000c831ec6bab9d8
鲨鱼辣椒 <monishine_code@163.com>
2025-04-23 17:27:28 +0800

    feat: 添加自定义返回类封装

---
+++ src/result.ts
@@ -0,0 +1,21 @@
+import { HttpStatus } from "@nestjs/common";
+
+export interface ResponseData<T> {
+  code: number;
+  msg: string;
+  data: T;
+}
+
+export class ResponseData<T> {
+  constructor(
+    public code: HttpStatus.OK,
+    public msg: string,
+    public data: T,
+  ) {
+    this.msg = msg || '操作成功';
+  }
+
+  static success<T>(data?: T, msg?: string) { }
+
+  static fail<T>(data?: T, msg?: string) { }
+}
