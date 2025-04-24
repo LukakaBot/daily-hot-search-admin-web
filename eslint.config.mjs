62d9019911f8cdba1fdc0437938eaa1829dead2b
鲨鱼辣椒 <monishine_code@163.com>
2025-04-24 14:27:19 +0800

    chore(deps-dev): 添加eslint-config-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+// import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   {
@@ -10,7 +11,8 @@ export default tseslint.config(
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  // eslintPluginPrettierRecommended,
+  eslintConfigPrettier,
   {
     languageOptions: {
       globals: {
