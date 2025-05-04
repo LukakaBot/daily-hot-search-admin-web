513fa84839a73c5443d5a3a7da25b6b7cbdb4b42
鲨鱼辣椒 <monishine_code@163.com>
2025-05-04 20:10:57 +0800

    feat: 添加b站热搜接口

--- src/modules/bilibili/bilibili.service.ts
+++ src/modules/bilibili/bilibili.service.ts
@@ -2,43 +2,104 @@ import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom, map } from 'rxjs';
 import { THeaders } from '@/typings/global';
-import { BilibiliApiResponse } from './interfaces/bilibili.interface';
+import { BilibiliApiResponse, BilibiliUserNavData, BilibiliVideoRankData } from './interfaces/bilibili.interface';
+import { createHash } from 'crypto';
+
+import { MD5 } from 'crypto-js';
 
 @Injectable()
 export class BilibiliService {
-  constructor(private readonly httpservice: HttpService) {
+  constructor(private readonly httpservice: HttpService) {}
 
-  }
+  md5 = createHash('md5');
+  MIXIN_KEY_ENC_TAB = [
+    46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49, 33, 9, 42, 19, 29, 28, 14, 39, 12, 38,
+    41, 13, 37, 48, 7, 16, 24, 55, 40, 61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11, 36,
+    20, 34, 44, 52,
+  ];
 
+  /** 对 imgKey 和 subKey 进行字符顺序打乱编码 */
   generateMixinKey(rawWbiKey: string) {
-    const MIXIN_KEY_ENC_TAB = [
-      46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
-      33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
-      61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
-      36, 20, 34, 44, 52
-    ];
-
-    return MIXIN_KEY_ENC_TAB.map((char) => rawWbiKey[char]).join("").slice(0, 32);
+    return this.MIXIN_KEY_ENC_TAB.map((char) => rawWbiKey[char])
+      .join('')
+      .slice(0, 32);
   }
 
-  async getWbiKeys() {
+  /** 为请求参数进行 wbi 签名 */
+  encWbi(params: { [key: string]: string | number | object }, imgKey: string, subKey: string) {
+    const mixinKey = this.generateMixinKey(imgKey + subKey),
+      currentTime = Math.round(Date.now() / 1000),
+      chrFilter = /[!'()*]/g;
+
+    Object.assign(params, { wts: currentTime }); // 添加 wts 字段
+    // 按照 key 重排参数
+    const query = Object.keys(params)
+      .sort()
+      .map((key) => {
+        // 过滤 value 中的 "!'()*" 字符
+        const value = params[key].toString().replace(chrFilter, '');
+        return `${encodeURIComponent(key)}=${encodeURIComponent(value)}`;
+      })
+      .join('&');
+
+    const wbiSign = this.md5.update(query + mixinKey).digest('hex'); // 计算 w_rid
+    const wbiSign2 = MD5(query + mixinKey).toString();
+    console.log('wbiSign');
+    console.log(wbiSign);
+    console.log('wbiSign2');
+    console.log(wbiSign2);
+
+    return query + '&w_rid=' + wbiSign;
+  }
+
+  /** 获取最新的 imgKey 和 subKey */
+  async getWbiKeys(SESSDATA: string = '') {
     const url = 'https://api.bilibili.com/x/web-interface/nav';
     const header: THeaders = {
-      'Cookie': 'SESSDATA=12345',
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
-      'Referer': 'https://www.bilibili.com/',
-    }
+      // SESSDATA 字段
+      Cookie: `SESSDATA=${SESSDATA}`,
+      'User-Agent':
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      Referer: 'https://www.bilibili.com/', // 对于直接浏览器调用可能不适用
+    };
 
-    const { data: { wbi_img: { img_url, sub_url } } } = await firstValueFrom(this.httpservice.get<BilibiliApiResponse>(url, { headers: header }).pipe(map(res => res.data)));
+    const {
+      data: {
+        wbi_img: { img_url, sub_url },
+      },
+    } = await firstValueFrom(
+      this.httpservice
+        .get<BilibiliApiResponse<BilibiliUserNavData>>(url, { headers: header })
+        .pipe(map((res) => res.data)),
+    );
+
     return {
-      img_key: img_url.split('/').pop()?.split('.')[0] || '',
-      sub_key: sub_url.split('/').pop()?.split('.')[0] || ''
+      imgKey: img_url.slice(img_url.lastIndexOf('/') + 1, img_url.lastIndexOf('.')),
+      subKey: sub_url.slice(sub_url.lastIndexOf('/') + 1, sub_url.lastIndexOf('.')),
     };
   }
 
+  /** 获取 wbi 签名 */
+  async getWbiSignParams() {
+    const webKeys = await this.getWbiKeys();
+    const params = { foo: '114', bar: '514', baz: 1919810 },
+      imgKey = webKeys.imgKey,
+      subKey = webKeys.subKey;
+    const query = this.encWbi(params, imgKey, subKey);
+    console.log(query);
+    return query;
+  }
+
   async getHotSearchData() {
-    const wbiKey = await this.getWbiKeys();
-    this.generateMixinKey(wbiKey.img_key);
-    return wbiKey;
+    const wbiSignParams = this.getWbiSignParams();
+    const url = `https://api.bilibili.com/x/web-interface/ranking/v2?tid=0&type=all&${wbiSignParams}`;
+
+    const {
+      data: { list },
+    } = await firstValueFrom(
+      this.httpservice.get<BilibiliApiResponse<BilibiliVideoRankData>>(url).pipe(map((res) => res.data)),
+    );
+
+    return list;
   }
 }
--- src/modules/bilibili/interfaces/bilibili.interface.ts
+++ src/modules/bilibili/interfaces/bilibili.interface.ts
@@ -8,10 +8,74 @@ export interface BilibiliUserNavData {
   wbi_img: BilibiliWbiImg;
 }
 
-export interface BilibiliApiResponse {
+export interface BilibiliVideoRankRights {
+  bp: number;
+  elec: number;
+  download: number;
+  movie: number;
+  pay: number;
+  hd5: number;
+  no_reprint: number;
+  autoplay: number;
+  ugc_pay: number;
+  is_cooperation: number;
+  ugc_pay_preview: number;
+  no_background: number;
+  arc_pay: number;
+  pay_free_watch: number;
+}
+
+export interface BilibiliVideoRankOwner {
+  mid: number;
+  name: string;
+  face: string;
+}
+
+export interface BilibiliVideoRankStat {
+  aid: number;
+  view: number;
+  danmaku: number;
+  reply: number;
+  favorite: number;
+  coin: number;
+  share: number;
+  now_rank: number;
+  his_rank: number;
+  like: number;
+  dislike: number;
+  vt: number;
+  vv: number;
+  fav_g: number;
+  like_g: number;
+}
+
+export interface BilibiliVideoRank {
+  aid: number;
+  videos: number;
+  tid: number;
+  tname: string;
+  copyright: number;
+  pic: string;
+  title: string;
+  pubdate: number;
+  ctime: number;
+  desc: string;
+  state: number;
+  duration: number;
+  mission_id: number;
+  rights: BilibiliVideoRankRights;
+  owner: BilibiliVideoRankOwner;
+  stat: BilibiliVideoRankStat;
+}
+
+export interface BilibiliVideoRankData {
+  note: string;
+  list: BilibiliVideoRank;
+}
+
+export interface BilibiliApiResponse<T = any> {
   code: number;
   message: string;
   ttl: number;
-  data: BilibiliUserNavData;
+  data: T;
 }
-
